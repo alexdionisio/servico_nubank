6a518bb60c73d40c8ee208ac831269b9f6f31118
alexdionisio <alexdionisio@outlook.com>
2020-04-14 01:31:38 -0300

    microservice/

---
+++ microservice/src/main/java/com/fiap/microservice/Controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.fiap.microservice.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * HelloController
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping(value="/")
+    public String getMethodName() {
+        return "Olá Nubank!";
+    }
+    
+}
